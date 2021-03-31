6973bca2aebaa575f5d602ff50900f7a7d854342
jamiejohnsonkc <jamiejohnsonkc@gmail.com>
2021-03-31 17:17:52 -0500

    moves array and random str created

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,14 @@
-console.log('Hello, World!')
+const movesArray = ['rock', 'paper', 'scissors']
+
+const rand = movesArray[Math.floor(Math.random() * movesArray.length)]
+
+function computerPlay() {
+    return rand
+}
+
+function singleRound(a,b) {
+    const playerSelection = a
+    const computerSelection = b
+}
+
+console.log(computerPlay())
