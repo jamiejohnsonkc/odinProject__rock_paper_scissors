80a9ee01c115b423e006a47acd1f9ef4b32a99e8
jamiejohnsonkc <jamiejohnsonkc@gmail.com>
2021-09-02 16:18:47 -0500

    removed comments

--- app.js
+++ app.js
@@ -18,16 +18,10 @@ Array.from(moves).forEach((move) => {
   })
 })
 
-// options = Array.from(moves)
 function cpuSelect() {
   var select = moves[Math.floor(Math.random() * moves.length)]
   computerSelection = select.innerText
-} // result.onclick = () => (result.style.display = 'none')
-
-// Array.from(options).forEach((move) => {
-//   var txt = move.textContent || move.innerText
-//   computerSelection = txt
-// })
+}
 
 function playRound() {
   cpuSelect()
@@ -67,37 +61,3 @@ function playRound() {
 function logScore() {
   console.log(`log score player ${player1Score} cpu ${cpuScore}`)
 }
-
-///////////////////////////////////
-
-// const results = document.querySelector('#results')
-// const scoreBox = document.createElement('div')
-// scoreBox.classList.add('scoreBox')
-// results.appendChild(scoreBox)
-
-// const subheading = document.querySelector('.scoreBox')
-// const subhead = document.createElement('h3')
-// subhead.textContent = 'The Score is'
-// subheading.appendChild(subhead)
-
-// const scores = document.createElement('div')
-// scores.classList.add('scores')
-// const scoresStyle = document.getElementsByTagName('h4')
-// scoreBox.appendChild(scores)
-// console.log(scoresStyle)
-
-// const playerLabel = document.createElement('h4')
-// playerLabel.textContent = 'You'
-// scores.appendChild(playerLabel)
-
-// const playerTally = document.createElement('h4')
-// playerTally.innerText = (`${window.player1Score}`)
-// scores.appendChild(playerTally)
-
-// const cpuLabel = document.createElement('h4')
-// cpuLabel.innerText = 'CPU'
-// scores.appendChild(cpuLabel)
-
-// const cpuTotal = document.createElement('h4')
-// cpuTotal.innerText = window.cpuScore
-// scores.appendChild(cpuTotal)
