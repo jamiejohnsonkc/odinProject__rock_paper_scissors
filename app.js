ce00da3eadd045255742131988f38589532a36be
jamiejohnsonkc <jamiejohnsonkc@gmail.com>
2021-09-02 12:42:13 -0500

    functionality restored

--- app.js
+++ app.js
@@ -1,125 +1,77 @@
-// creates node list
-const moveBtns = document.querySelectorAll('.moveBtn')
-const rockBtn = document.querySelector('#rockBtn')
-const paperBtn = document.querySelector('#paperBtn')
-const scissorsBtn = document.querySelector('#scissorsBtn')
-
+const moves = document.getElementsByClassName('move')
 let playerSelection = ''
+let computerSelection = ''
+let player1Score = 0
+let cpuScore = 0
 
+console.log(moves)
 
-// iterate through the node list items
-moveBtns.forEach((button) => {
-  button.addEventListener('click', (e) => {
-    console.log(e)
-    if (button.textContent === 'rock') {
-      playerSelection = 'rock'
-    }
-    if (button.textContent === 'paper') {
-      playerSelection = 'paper'
-    }
-    if (button.textContent === 'scissors') {
-      playerSelection = 'scissors'
-    }
-    playRound()
+Array.from(moves).forEach((move) => {
+  move.addEventListener('click', (e) => {
+    playerSelection = move.innerText
+    console.log(`event ${playerSelection} ${computerSelection}`)
+    playRound(playerSelection, computerSelection)
     logScore()
- 
   })
 })
 
-function choosePcMove() {
-  const moves = ['rock', 'paper', 'scissors']
-  const random = Math.floor(Math.random() * moves.length)
-  return moves[random]
-}
+Array.from(moves).forEach((move) => {
+  var txt = move.textContent || move.innerText
+  computerSelection = txt
+})
 
-
-function logScore () {
-  var playerScore = 0
-  var computerScore = 0
-  console.log(`end ${playerScore} and ${computerScore}`)
-}
+var elem = document.getElementById('rock')
+var txt = elem.textContent || elem.innerText
 
 function playRound() {
-  const computerSelection = choosePcMove()
-
- 
-  if (playerSelection === 'rock' && computerSelection === 'paper') {
-    // console.log(playerScore, computerScore)
-    computerScore++
-    // return computerScore
-    // return console.log(
-    //   `Paper beats rock. You lose! The score is player ${playerScore} computer ${computerScore}`
-    // )
-  } if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    playerScore++
-    // return playerScore
-    // return console.log(
-    //   `Rock beats scissors. You win! The score is player ${playerScore} computer ${computerScore}`
-    // )
-  } if (playerSelection === 'paper' && computerSelection === 'rock') {
-    playerScore++
-    // return playerScore
-    // return console.log(
-    //   `Paper beats rock. You win! The score is player ${playerScore} computer ${computerScore}`
-    // )
-  } if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    computerScore++
-    // return computerScore
-    // return console.log(
-    //   `Scissors beat paper. You lose! The score is player ${playerScore} computer ${computerScore}`
-    // )
-  } if (playerSelection === 'scisssors' && computerSelection === 'rock') {
-    computerScore++
-    // return computerScore
-    // return console.log(
-    //   `Rock beats scissors. You lose! The score is player ${playerScore} computer ${computerScore}`
-    // )
-  } if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    playerScore++
-    // return playerScore
-    // return console.log(
-    //   `Scissors beat paper. You win! The score is player ${playerScore} computer ${computerScore}`
-    // )
+  console.log(`playround ${playerSelection} ${computerSelection}`)
+  if (playerSelection === computerSelection) {
+    console.log('tie!')
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    player1Score++
   } else {
-    // return console.log(
-    //   `It's a tie! The score is player ${playerScore} computer ${computerScore}`
-    // )
+    cpuScore++
   }
-console.log(playerScore, computerScore)
-return {playerScore, computerScore}
 }
 
+function logScore() {
+  console.log(`log score player ${player1Score} cpu ${cpuScore}`)
+}
 
+///////////////////////////////////
 
-const results = document.querySelector('#results')
-const scoreBox = document.createElement('div')
-scoreBox.classList.add('scoreBox')
-results.appendChild(scoreBox)
+// const results = document.querySelector('#results')
+// const scoreBox = document.createElement('div')
+// scoreBox.classList.add('scoreBox')
+// results.appendChild(scoreBox)
 
-const subheading = document.querySelector('.scoreBox')
-const subhead = document.createElement('h3')
-subhead.textContent = 'The Score is'
-subheading.appendChild(subhead)
+// const subheading = document.querySelector('.scoreBox')
+// const subhead = document.createElement('h3')
+// subhead.textContent = 'The Score is'
+// subheading.appendChild(subhead)
 
-const scores = document.createElement('div')
-scores.classList.add('scores')
-const scoresStyle = document.getElementsByTagName('h4')
-scoreBox.appendChild(scores)
-console.log(scoresStyle)
+// const scores = document.createElement('div')
+// scores.classList.add('scores')
+// const scoresStyle = document.getElementsByTagName('h4')
+// scoreBox.appendChild(scores)
+// console.log(scoresStyle)
 
-const playerLabel = document.createElement('h4')
-playerLabel.textContent = 'You'
-scores.appendChild(playerLabel)
+// const playerLabel = document.createElement('h4')
+// playerLabel.textContent = 'You'
+// scores.appendChild(playerLabel)
 
-const playerTally = document.createElement('h4')
-playerTally.innerText = (`${window.playerScore}`)
-scores.appendChild(playerTally)
+// const playerTally = document.createElement('h4')
+// playerTally.innerText = (`${window.player1Score}`)
+// scores.appendChild(playerTally)
 
-const cpuLabel = document.createElement('h4')
-cpuLabel.innerText = 'CPU'
-scores.appendChild(cpuLabel)
+// const cpuLabel = document.createElement('h4')
+// cpuLabel.innerText = 'CPU'
+// scores.appendChild(cpuLabel)
 
-const cpuTotal = document.createElement('h4')
-cpuTotal.innerText = window.computerScore
-scores.appendChild(cpuTotal)
-
+// const cpuTotal = document.createElement('h4')
+// cpuTotal.innerText = window.cpuScore
+// scores.appendChild(cpuTotal)
