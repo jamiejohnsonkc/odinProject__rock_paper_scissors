178ee7c792502837ee01b5d111e2c5acf0adc549
jamiejohnsonkc <jamiejohnsonkc@gmail.com>
2021-09-02 13:29:06 -0500

    functioning

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let playerSelection = ''
 let computerSelection = ''
 let player1Score = 0
 let cpuScore = 0
+let score = document.querySelector('#score')
+let result = document.querySelector('#result')
+let outcome = document.querySelector('#outcome')
 
 console.log(moves)
 
@@ -10,31 +13,54 @@ Array.from(moves).forEach((move) => {
   move.addEventListener('click', (e) => {
     playerSelection = move.innerText
     console.log(`event ${playerSelection} ${computerSelection}`)
-    playRound(playerSelection, computerSelection)
+    playRound()
     logScore()
   })
 })
 
-Array.from(moves).forEach((move) => {
-  var txt = move.textContent || move.innerText
-  computerSelection = txt
-})
+// options = Array.from(moves)
+function cpuSelect() {
+  var select = moves[Math.floor(Math.random() * moves.length)]
+  computerSelection = select.innerText
+} // result.onclick = () => (result.style.display = 'none')
 
-var elem = document.getElementById('rock')
-var txt = elem.textContent || elem.innerText
+// Array.from(options).forEach((move) => {
+//   var txt = move.textContent || move.innerText
+//   computerSelection = txt
+// })
 
 function playRound() {
+  cpuSelect()
   console.log(`playround ${playerSelection} ${computerSelection}`)
   if (playerSelection === computerSelection) {
-    console.log('tie!')
+    result.style.display = 'block'
+    result.textContent =
+      'You both chose' + ' ' + playerSelection + '. ' + 'lame.'
   } else if (
     (playerSelection === 'rock' && computerSelection === 'scissors') ||
     (playerSelection === 'paper' && computerSelection === 'rock') ||
     (playerSelection === 'scissors' && computerSelection === 'paper')
   ) {
+    result.style.display = 'block'
+    result.textContent = 'You chose wisely.  Well done.'
     player1Score++
   } else {
+    result.style.display = 'block'
+    result.textContent = 'You lose fuckface. Try again.'
     cpuScore++
+  }
+  score.style.display = 'block'
+  score.textContent = 'The score is ' + player1Score + ' to ' + cpuScore + '.'
+  if (cpuScore === 5) {
+    result.textContent = 'game over man. game over.'
+    result.onclick = () => {
+      window.location.reload(false)
+    }
+  } else if (player1Score === 5) {
+    result.textContent = 'you are victorious'
+    result.onclick = () => {
+      window.location.reload(false)
+    }
   }
 }
 
